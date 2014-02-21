80062ec7d7cf7d952d784ec50c9df4263d0b85af
Isis Van Parijs <isis.vanparijs@gmail.com>
2014-02-21 17:31:02 +0100

    update

--- macros/FCNC_4L5L.C
+++ macros/FCNC_4L5L.C
@@ -13,7 +13,9 @@ void FCNC_4L5L(){
 
   TopAnalyzerLite* analyzer = new TopAnalyzerLite(treeName, imageOutDir);
   
-  analyzer->addMCSig("Hct", "Hct", "45_TTJetsTocHbW_HToZZ_ZToLL_HctL_tree.root", 5, 555, kRed, false);
+  analyzer->addMCSig("Hct", "Hct", "45_TTJetsTocHbW_HToZZ_ZToLL_HctL_tree.root", 5, -1, kRed, false,"");
+  
+  analyzer->addMCBkg("ggF", "ggF", "45__GluGluHiggs4lep_tree.root", 19.27, -1, kBlue, "");
   
   analyzer->addMonitorPlot("nJets", "nJets", "Jet Multiplicity;Jet Multiplicity;Events", 15, 0, 15, 0.5, 2000, true);
 
