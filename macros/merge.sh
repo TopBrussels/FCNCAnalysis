669ffd9cf0c6f6bcdfbf8de9b2e9bff751397acb
Isis Van Parijs <isis.vanparijs@gmail.com>
2014-03-03 14:21:02 +0100

    merge datasamples

---
+++ macros/merge.sh
@@ -0,0 +1,14 @@
+rm ../ntuples/45_data_MuEG_tree.root
+hadd -f ../ntuples/45_data_MuEG_tree.root ../ntuples/45_data_MuEG_Run*_tree.root 
+
+
+rm ../ntuples/45_data_DoubleMuParked_tree.root
+hadd -f ../ntuples/45_data_DoubleMuParked_tree.root ../ntuples/45_data_DoubleMuParked_Run*_tree.root 
+
+
+rm ../ntuples/3L_data_MuEG_tree.root
+hadd -f ../ntuples/3L_data_MuEG_tree.root ../ntuples/3L_data_MuEG_Run*_tree.root 
+
+
+rm ../ntuples/3L_data_DoubleMuParked_tree.root
+hadd -f ../ntuples/3L_data_DoubleMuParked_tree.root ../ntuples/3L_data_DoubleMuParked_Run*_tree.root 
