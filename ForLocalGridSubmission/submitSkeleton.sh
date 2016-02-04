abf6299b9d29d23427e8f63005ece1b673ac245f
sabuzeid <sh.abuzeid@gmail.com>
2016-02-04 17:50:34 +0100

    adapted to work with 76X

---
+++ ForLocalGridSubmission/submitSkeleton.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#PBS -q localgrid
+#PBS -l walltime=02:00:00
+
+# setting up your code and your env
+source /user/sabuzeid/.bash_login
+cd /localgrid/sabuzeid/FCNC_Study_LocalGrid/CMSSW_7_6_3/src/
+#cmsenv
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+eval `scramv1 runtime -sh`
+cd TopBrussels/FCNCAnalysis/
+# want you really want to do!!
