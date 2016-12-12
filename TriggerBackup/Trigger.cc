01d3aea6c89ab38a23cb303ff11ad19f58b0c0f1
Isis Van Parijs <isis.vanparijs@gmail.com>
2016-12-12 15:59:27 +0100

    backup

---
+++ TriggerBackup/Trigger.cc
@@ -0,0 +1,329 @@
+
+#include "../interface/Trigger.h"
+
+Trigger::Trigger(bool isMuon, bool isElectron, bool trigSingleLep, bool trigDoubleLep, bool trigTrilep):
+muon(false), electron(false), singleLep(false), doubleLep(false), trilep(false), fullHadr(false), trigged(false), redotrigmap(false), triggerList(), currentRunTrig(0), previousRunTrig(-1), currentFilenameTrig(""), previousFilenameTrig(""), iFileTrig(-1), treeNumberTrig(-1), triggermap()
+{
+  if (isMuon)
+  {
+    muon = true;
+  }
+  if (isElectron)
+  {
+    electron = true;
+  }
+  else if(!isMuon && !isElectron)
+  {
+    cout << "TRIGGER::TRIGGER - No selected lepton..." << endl;
+  }
+  if (trigSingleLep)
+  {
+    singleLep = true;
+  }
+  if (trigDoubleLep)
+  {
+    doubleLep = true;
+  }
+  if(trigTrilep){
+    trilep = true;
+  }
+  if (! trigSingleLep && ! trigDoubleLep && !trigTrilep)
+  {
+    fullHadr = true;
+    cout << "TRIGGER::TRIGGER - Do you really want no lepton triggers?" << endl;
+  }
+}
+
+Trigger::~Trigger()
+{
+  
+}
+
+void Trigger::bookTriggers(bool isData, string dName)
+{
+  /// This function is called in the dataset loop
+  //  Reset all quantities here
+  triggerList.clear();
+  currentRunTrig = 0;
+  previousRunTrig = -1;
+  currentFilenameTrig = previousFilenameTrig = "";
+  iFileTrig = -1;
+  treeNumberTrig = -1;
+  triggermap.clear();
+  
+  /// Add relevant triggers to triggerlist
+  //  Recommended triggers for TOP analyses
+  //  Last updated: 29 June 2016. https://twiki.cern.ch/twiki/bin/viewauth/CMS/TopTrigger
+  if (singleLep && !doubleLep && !trilep)
+  {
+    if (muon)
+    {
+      if (isData)
+      {
+        if( dName.find("Data_Run2016B")!=string::npos || dName.find("Data_Run2016C")!=string::npos || dName.find("Data_Run2016D")!=string::npos || dName.find("Data_Run2016E")!=string::npos || dName.find("Data_Run2016F")!=string::npos)
+        {
+          triggerList.push_back("HLT_IsoMu22_v*");
+          triggerList.push_back("HLT_IsoTkMu22_v*");
+//          triggerList.push_back("HLT_IsoMu24_v*");
+//          triggerList.push_back("HLT_IsoTkMu24_v*");
+        }
+        else if( dName.find("Data_Run2015C")!=string::npos || dName.find("Data_Run2015D")!=string::npos ){
+          triggerList.push_back("HLT_IsoMu18_v*");  // Data
+          //           triggerList.push_back("HLT_IsoTkMu2_v*");
+        }
+        else cout << "ERROR: no correct data triggers selected" << endl;
+      }
+      else
+      {
+        if( dName.find("80X")!=string::npos)
+        {
+          triggerList.push_back("HLT_IsoMu24_v2");
+          triggerList.push_back("HLT_IsoTkMu24_v2");
+        }
+        else if( dName.find("76X")!=string::npos ){
+          triggerList.push_back("HLT_IsoMu18_v*");
+          //           triggerList.push_back("HLT_IsoTkMu20_v*");
+        }
+        else cout << "ERROR: no correct MC triggers selected" << endl;
+      }
+    }
+    if (electron)
+    {
+      if (isData)
+      {
+        
+        if( dName.find("Data_Run2016B")!=string::npos || dName.find("Data_Run2016C")!=string::npos || dName.find("Data_Run2016D")!=string::npos || dName.find("Data_Run2016E")!=string::npos || dName.find("Data_Run2016F")!=string::npos)
+        {
+          triggerList.push_back("HLT_Ele32_eta2p1_WPTight_Gsf_v*");
+        }
+        else if( dName.find("Data_Run2015C")!=string::npos || dName.find("Data_Run2015D")!=string::npos ){
+          triggerList.push_back("HLT_Ele23_WPLoose_Gsf_v*");  // Data, restricted to eta < 2.1
+        }
+        else cout << "ERROR: no correct data triggers selected" << endl;
+        
+      }
+      else
+      {
+        if( dName.find("80X")!=string::npos)
+        {
+          triggerList.push_back("HLT_Ele32_eta2p1_WPTight_Gsf_v3");
+        }
+        else if( dName.find("76X")!=string::npos){
+          triggerList.push_back("HLT_Ele23_WPLoose_Gsf_v*");
+        }
+        else cout << "ERROR: no correct MC triggers selected" << endl;
+      }
+    }
+  }
+  
+  if (doubleLep && !singleLep && !trilep)   // Triggers for data & MC are the same
+  {
+    //cout << "muon " << muon << " electron " << electron << endl;
+    if (muon && !electron)
+    {
+      if (isData)
+      {
+        
+        if( dName.find("Data_Run2016B")!=string::npos || dName.find("Data_Run2016C")!=string::npos || dName.find("Data_Run2016D")!=string::npos || dName.find("Data_Run2016E")!=string::npos || dName.find("Data_Run2016F")!=string::npos)
+        {
+          triggerList.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v*");
+ 	  triggerList.push_back("HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v*");
+        }
+        else if( dName.find("Data_Run2015C")!=string::npos || dName.find("Data_Run2015D")!=string::npos ){
+          triggerList.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v*");  // Data, restricted to eta < 2.1
+        }
+        else cout << "ERROR: no correct data triggers selected" << endl;
+        
+      }
+      else
+      {
+        if( dName.find("80X")!=string::npos)
+        {
+          triggerList.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v3");
+        }
+        else if( dName.find("76X")!=string::npos ){
+          triggerList.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v*");
+        }
+        else cout << "ERROR: no correct MC triggers selected" << endl;
+      }
+    }
+    else if (electron && !muon)
+    {
+      if (isData)
+      {
+        
+        if( dName.find("Data_Run2016B")!=string::npos || dName.find("Data_Run2016C")!=string::npos || dName.find("Data_Run2016D")!=string::npos || dName.find("Data_Run2016E")!=string::npos || dName.find("Data_Run2016F")!=string::npos)
+        {
+          triggerList.push_back("HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*");
+        }
+        else if( dName.find("Data_Run2015C")!=string::npos || dName.find("Data_Run2015D")!=string::npos ){
+          triggerList.push_back("HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*");  // Data, restricted to eta < 2.1
+        }
+        else cout << "ERROR: no correct data triggers selected" << endl;
+        
+      }
+      else
+      {
+        if( dName.find("80X")!=string::npos)
+        {
+          triggerList.push_back("HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v4");
+        }
+        else if( dName.find("76X")!=string::npos) {
+          triggerList.push_back("HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*");
+        }
+        else cout << "ERROR: no correct MC triggers selected" << endl;
+      }
+    }
+    
+    else if (muon && electron)
+    {
+      if (isData)
+      {
+        
+        if( dName.find("Data_Run2016B")!=string::npos || dName.find("Data_Run2016C")!=string::npos || dName.find("Data_Run2016D")!=string::npos || dName.find("Data_Run2016E")!=string::npos || dName.find("Data_Run2016F")!=string::npos)
+        {
+          triggerList.push_back("HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v*");
+          triggerList.push_back("HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v*");
+          triggerList.push_back("HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*");
+          triggerList.push_back("HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*");
+          
+        }
+        else if( dName.find("Data_Run2015C")!=string::npos || dName.find("Data_Run2015D")!=string::npos ){
+          triggerList.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v*");
+        }
+        else cout << "ERROR: no correct data triggers selected" << endl;
+        
+      }
+      else
+      {
+        if( dName.find("80X")!=string::npos)
+        {
+          triggerList.push_back("HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v4");
+          triggerList.push_back("HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v4");
+        }
+        else if( dName.find("76X")!=string::npos ){
+          triggerList.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v*");
+        }
+        else cout << "ERROR: no correct MC triggers selected" << endl;
+      }
+    }
+    
+  }
+  if(trilep){
+    if(muon && !electron)
+    {
+      
+      if (isData)
+      {
+        
+        if( dName.find("Data_Run2016B")!=string::npos || dName.find("Data_Run2016C")!=string::npos || dName.find("Data_Run2016D")!=string::npos || dName.find("Data_Run2016E")!=string::npos || dName.find("Data_Run2016F")!=string::npos)
+        {
+          triggerList.push_back("HLT_TripleMu_12_10_5_v*");
+          triggerList.push_back("HLT_TripleMu_5_3_3_v*");
+          
+        }
+        else if( dName.find("Data_Run2015C")!=string::npos || dName.find("Data_Run2015D")!=string::npos ){
+           triggerList.push_back("HLT_TripleMu_12_10_5_v*");
+          
+        
+        }
+        else cout << "ERROR: no correct data triggers selected" << endl;
+        
+      }
+      else
+      {
+        if( dName.find("80X")!=string::npos)
+        {
+          triggerList.push_back("HLT_TripleMu_12_10_5_v*");
+          triggerList.push_back("HLT_TripleMu_5_3_3_v*");
+        }
+        else if( dName.find("76X")!=string::npos ){
+          triggerList.push_back("HLT_TripleMu_12_10_5_v*");
+
+        }
+        else cout << "ERROR: no correct MC triggers selected" << endl;
+      }
+    }
+    
+
+    else if(!muon && electron)
+    {
+      triggerList.push_back("HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL_v*");
+      
+    }
+    else if(muon && electron)
+    {
+      triggerList.push_back("HLT_DiMu9_Ele9_CaloIdL_TrackIdL_v*");
+      triggerList.push_back("HLT_Mu8_DiEle12_CaloIdL_TrackIdL_v*");
+      
+    }   
+
+
+
+ 
+  }
+  for(UInt_t iTrig = 0; iTrig < triggerList.size(); iTrig++)
+  {
+    triggermap[triggerList[iTrig]] = std::pair<int,bool> (-999,false);
+  }
+  
+}
+
+void Trigger::checkAvail(int currentRunTrig, vector < Dataset* > datasets, unsigned int d, TTreeLoader *treeLoader, TRootEvent* event, bool verbose)
+{
+  redotrigmap = false;
+  treeNumberTrig = datasets[d]->eventTree()->GetTreeNumber();
+  currentFilenameTrig = datasets[d]->eventTree()->GetFile()->GetName();
+  if (previousFilenameTrig != currentFilenameTrig)
+  {
+    previousFilenameTrig = currentFilenameTrig;
+    iFileTrig++;
+    redotrigmap = true;
+    cout << endl << "*****File changed!!! => iFile = " << iFileTrig << " new file is " << currentFilenameTrig << " in sample " << datasets[d]->Name() << " *****" << endl;
+  }
+  if (previousRunTrig != currentRunTrig)
+  {
+    previousRunTrig = currentRunTrig;
+    cout << "*****Run changed!!! => new run = " << previousRunTrig << " *****" << endl;
+    redotrigmap=true;
+  }
+  
+  if (verbose && redotrigmap)
+  {
+    treeLoader->ListTriggers(currentRunTrig, treeNumberTrig);
+  }
+  
+  
+  // get trigger info:
+  for(std::map<std::string,std::pair<int,bool> >::iterator iter = triggermap.begin(); iter != triggermap.end(); iter++)
+  {
+    if (redotrigmap)
+    {
+      Int_t loc = treeLoader->iTrigger(iter->first, currentRunTrig, treeNumberTrig);
+      string trigname = iter->first;
+      cout << "trigname: " << trigname << "  location: " << loc << /*"  " << currentFilenameTrig << "  " << currentRunTrig <<*/ endl;
+      
+      iter->second.first = loc;
+    }
+    // and check if it exists and if it fired:
+    if (iter->second.first >= 0 && iter->second.first != 9999) // trigger exists
+      iter->second.second = event->trigHLT(iter->second.first);
+    else
+      iter->second.second = false;
+  }
+}
+
+
+int Trigger::checkIfFired()
+{
+  // now check if the appropriate triggers fired for each analysis:
+  trigged = 0;
+  
+  for(UInt_t itrig = 0; itrig < triggerList.size() && trigged == 0; itrig++)
+  {
+    if (triggermap[triggerList[itrig]].second)
+      trigged = 1;
+  }
+  
+  return trigged;
+}
---
+++ TriggerBackup/Trigger.h
@@ -0,0 +1,54 @@
+#ifndef INTERFACE_TRIGGER_H
+#define INTERFACE_TRIGGER_H
+
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <sys/stat.h>
+#include <errno.h>
+#include <iostream>
+#include <map>
+
+//TopTree classes
+#include "TopTreeProducer/interface/TRootRun.h"
+#include "TopTreeProducer/interface/TRootEvent.h"
+
+//TopTreeAnalysisBase classes
+#include "TopTreeAnalysisBase/Content/interface/AnalysisEnvironment.h"
+#include "TopTreeAnalysisBase/Content/interface/Dataset.h"
+#include "TopTreeAnalysisBase/Tools/interface/PlottingTools.h"
+#include "TopTreeAnalysisBase/Tools/interface/MultiSamplePlot.h"
+#include "TopTreeAnalysisBase/Tools/interface/TTreeLoader.h"
+#include "TopTreeAnalysisBase/Tools/interface/AnalysisEnvironmentLoader.h"
+#include "TopTreeAnalysisBase/Selection/interface/Run2Selection.h"
+
+
+class Trigger{
+	public:
+		Trigger(bool isMuon, bool isElectron, bool trigSingleLep, bool trigDoubleLep, bool trigTrilep);
+		~Trigger();
+		void bookTriggers(bool isData, string dataName);
+		void checkAvail(int currentRunTrig, vector<Dataset*> datasets, unsigned int d, TTreeLoader* treeLoader, TRootEvent* event, bool verbose);
+		int checkIfFired();
+
+
+	private:
+		bool muon;
+		bool electron;
+    bool singleLep;
+    bool doubleLep;
+    bool trilep;
+    bool fullHadr;
+		bool trigged;
+		bool redotrigmap;
+	  std::vector<std::string> triggerList;
+	  int currentRunTrig;
+	  int previousRunTrig;
+		string currentFilenameTrig;
+		string previousFilenameTrig;
+		int iFileTrig;
+    int treeNumberTrig;
+	  std::map<std::string,std::pair<int,bool> > triggermap;
+};
+
+#endif
