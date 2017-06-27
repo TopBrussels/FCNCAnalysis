f1cf6934387dd3e1747cf77775299ab1f4dd1c97
Isis Van Parijs <isis.vanparijs@gmail.com>
2017-06-27 15:09:39 +0200

    update

---
+++ Optimal_cut.C
@@ -0,0 +1,376 @@
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <sys/stat.h>
+#include <TFile.h>
+#include <TH1.h>
+#include <iostream>
+#include <TCanvas.h>
+#include <vector>
+#include "TGraph.h"
+#include "TVectorT.h"
+
+using namespace std;
+
+string infile = "OutputPlots/170620_1629/NtuplePlots.root";//1543
+string eclusiveinfile = "OutputPlots/170620_1629/NtuplePlots.root";
+bool debug = false;
+int jetcut = 5 + 1; //+1 for getting the right binnumber
+int bcut = 3 +1; //+1 for the binnumber
+
+void Optimal_cut(){
+  
+  TFile *file = new TFile(infile.c_str(),"read");
+  TFile *exclusiveFile = new TFile(eclusiveinfile.c_str(),"read");
+  TFile *outputfile = new TFile("NtuplePlotsTest.root","RECREATE");
+  
+  vector <string> Variables;
+		
+  Variables.push_back("DeltaEtaWlepB3lep");
+  Variables.push_back("DeltaEtaWlepZ3lep");
+  Variables.push_back("DeltaEtaWlepSMtop3lep");
+  Variables.push_back("d0muon3lep");
+  Variables.push_back("d0electron3lep");
+  Variables.push_back("d0all3lep");
+  Variables.push_back("DeltaRWlepB3lep");
+  Variables.push_back("DeltaRWlepZ3lep");
+  Variables.push_back("DeltaRWlepSMtop3lep");
+  
+  
+  
+  vector <string> signalname;
+  vector <string> backgroundnames;
+		
+  backgroundnames.push_back("fake_80X");
+  
+  
+  signalname.push_back("NP_overlay_ST_FCNC_zct_80X");
+  
+  for(unsigned int iVar = 0; iVar<Variables.size(); iVar++){
+    
+    TDirectory* th1dir = outputfile->mkdir(Variables[iVar].c_str());
+    th1dir->cd();
+    
+    
+    vector <TH1F*> Histo_samples;
+    vector <string> Name_samples;
+    
+    string Path_To_Histo = "1D_histograms_fakedisc";
+    //Path_To_Histo += Variables[iVar];
+    Path_To_Histo += "/";
+    Path_To_Histo += Variables[iVar];
+    Path_To_Histo += "_all";
+    
+    int first_index_S = -1; // Define the first indices for S and B, which indicate which histograms are filled and which not from all samples
+    int first_index_B = -1;
+    
+    TH1F *HistoSignal = 0;
+    for(unsigned int iSignal = 0; iSignal < signalname.size(); iSignal++){
+      
+      cout << iSignal << endl;
+      
+      if(HistoSignal) continue; //We only want to go further if the HistoSignal isn't filled yet
+      
+      string histoName = Path_To_Histo;
+      histoName += signalname[iSignal];
+      TH1F *histo( (TH1F*) file->Get(histoName.c_str()) );
+      TH1F *histo_exclusive( (TH1F*) exclusiveFile->Get(histoName.c_str()) );
+      
+      
+      if(histo){
+        //histo->Clone("Hist");
+        HistoSignal = histo;
+        first_index_S = iSignal;
+        Histo_samples.push_back(histo);
+        Name_samples.push_back(signalname[iSignal]);
+        
+        if(debug){
+          cout << histo->Integral(6,15) << endl;
+          cout << Histo_samples.back()->Integral(6,15) << endl;
+          cout << " " << endl;
+        }
+        continue;
+      }
+     /* if(histo_exclusive){
+        //histo_exclusive->Clone("Histo");
+        HistoSignal = histo_exclusive;
+        first_index_S = iSignal;
+        Histo_samples.push_back(histo_exclusive);
+        Name_samples.push_back(signalname[iSignal]);
+        
+        if(debug){
+          cout << histo_exclusive->Integral(6,15) << endl;
+          cout << Histo_samples.back()->Integral(6,15) << endl;
+          cout << " " << endl;
+        }
+      }*/
+      
+    }
+    TH1F *HistoBackground = 0;
+    for(unsigned int iBackgr = 0; iBackgr < backgroundnames.size(); iBackgr++){
+      //cout << iBackgr << endl;
+      
+      if(HistoBackground) continue; //We only want to go further if the HistoBackground isn't filled yet
+      string histoName = Path_To_Histo;
+      histoName += backgroundnames[iBackgr];
+      TH1F *histo( (TH1F*) file->Get(histoName.c_str()) );
+      TH1F *histo_exclusive( (TH1F*) exclusiveFile->Get(histoName.c_str()) );
+      
+      if(histo){
+        //histo->Clone("Histog");
+        HistoBackground = histo;
+        first_index_B = iBackgr;
+        Histo_samples.push_back(histo);
+        Name_samples.push_back(backgroundnames[iBackgr]);
+        
+        if(debug){
+          cout << histo->Integral(6,15) << endl;
+          cout << Histo_samples.back()->Integral(6,15) << endl;
+          cout << " " << endl;
+        }
+        continue;
+      }
+      /*if(histo_exclusive){
+        //histo_exclusive->Clone("Histogr");
+        HistoBackground = histo_exclusive;
+        first_index_B = iBackgr;
+        Histo_samples.push_back(histo_exclusive);
+        Name_samples.push_back(backgroundnames[iBackgr]);
+        
+        if(debug){
+          cout << histo_exclusive->Integral(6,15) << endl;
+          cout << Histo_samples.back()->Integral(6,15) << endl;
+          cout << " " << endl;
+        }
+      }*/
+    }
+    
+    
+    //cout << "HERE" << endl;
+    //Add the histograms into 1 for S and B seperately
+    for(unsigned int iSignal = first_index_S+1; iSignal < signalname.size(); iSignal++){
+      
+      //cout << iSignal << endl;
+      string histoName = Path_To_Histo;
+      histoName += signalname[iSignal];
+      TH1F *histo( (TH1F*) file->Get(histoName.c_str()) );
+      TH1F *histo_exclusive( (TH1F*) exclusiveFile->Get(histoName.c_str()) );
+      
+      if(histo){
+        HistoSignal->Add( histo );
+        Histo_samples.push_back(histo);
+        Name_samples.push_back(signalname[iSignal]);
+        
+        if(debug){
+          cout << histo->Integral(6,15) << endl;
+          cout << Histo_samples.back()->Integral(6,15) << endl;
+          cout << " " << endl;
+        }
+        continue;
+      }
+      /*if(histo_exclusive){
+        HistoSignal->Add( histo_exclusive );
+        Histo_samples.push_back(histo_exclusive);
+        Name_samples.push_back(signalname[iSignal]);
+        
+        if(debug){
+          cout << histo_exclusive->Integral(6,15) << endl;
+          cout << Histo_samples.back()->Integral(6,15) << endl;
+          cout << " " << endl;
+        }
+      }*/
+    }
+    
+    
+    for(unsigned int iBackgr = first_index_B+1; iBackgr < backgroundnames.size(); iBackgr++){
+      
+      //cout << iBackgr << endl;
+      string histoName = Path_To_Histo;
+      histoName += backgroundnames[iBackgr];
+      TH1F *histo( (TH1F*) file->Get(histoName.c_str()) );
+      TH1F *histo_exclusive((TH1F*) exclusiveFile->Get(histoName.c_str()));
+      
+      if(histo){
+        HistoBackground->Add( histo );
+        Histo_samples.push_back(histo);
+        Name_samples.push_back(backgroundnames[iBackgr]);
+        
+        if(debug){
+          cout << histo->Integral(6,15) << endl;
+          cout << Histo_samples.back()->Integral(6,15) << endl;
+          cout << " " << endl;
+        }
+        continue;
+      }
+     /* if(histo_exclusive){
+        HistoBackground->Add( histo_exclusive );
+        Histo_samples.push_back(histo_exclusive);
+        Name_samples.push_back(backgroundnames[iBackgr]);
+        
+        if(debug){
+          cout << histo_exclusive->Integral(6,15) << endl;
+          cout << Histo_samples.back()->Integral(6,15) << endl;
+          cout << " " << endl;
+        }
+      }*/
+    }
+    
+    //cout << "HERE" << endl;
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    //// Now we get into the real part where we find the optimal cut, where the significance is the highest
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    //cout << HistoSignal << endl;
+    int end = HistoSignal->GetNbinsX();
+    //cout << "HERE 3" << endl;
+    //Efficiencies calculating as #events_passing_cut/#Total_events
+    double Total_signal = 0;
+    double Total_background = 0;
+    Total_signal = HistoSignal->Integral();
+    Total_background = HistoBackground->Integral();
+    
+    double * Signal_Integral_PerBin = new double [end];
+    double * Background_Integral_PerBin = new double [end];
+    double * Eff_Signal = new double [end];
+    double * RejectionEff_Background = new double [end];
+    
+    for(unsigned int i = 1; i< end; i++){
+      double s = 0;
+      double b = 0;
+      //cout << i << endl;
+      s = (HistoSignal->Integral(1 , i));
+      b = (HistoBackground->Integral(1,i));
+
+      Signal_Integral_PerBin[i] = s;
+      Background_Integral_PerBin[i] = b;
+      Eff_Signal[i] = s/Total_signal;
+      RejectionEff_Background[i] = (1- b/Total_background);
+    }
+    
+    
+    
+    //Determine the optimal cut-value for a cut-and-count experiment
+    string optcutName = "Opt_cut";
+    TH1F *Opt_cut = new TH1F(optcutName.c_str(),optcutName.c_str(), end, HistoSignal->GetXaxis()->GetXmin(), HistoSignal->GetXaxis()->GetXmax());
+    for(unsigned int i = 0; i<end; i++){
+      double signal_significance = Signal_Integral_PerBin[i]/sqrt(Background_Integral_PerBin[i]);
+      if(Background_Integral_PerBin[i] == 0) signal_significance = 1;
+      
+      Opt_cut->SetBinContent(i, signal_significance);
+    }
+    Opt_cut->GetXaxis()->SetTitle(Variables[iVar].c_str());
+    Opt_cut->GetYaxis()->SetTitle("Signif.");
+    
+    string signaleeffName = "Signal_eff";
+    TH1F *Signal_eff = new TH1F(signaleeffName.c_str(),signaleeffName.c_str(), end, HistoSignal->GetXaxis()->GetXmin(), HistoSignal->GetXaxis()->GetXmax());
+    for(unsigned int i = 0; i<end; i++){
+      Signal_eff->SetBinContent(i, Eff_Signal[i]);
+    }
+    Signal_eff->GetXaxis()->SetTitle(Variables[iVar].c_str());
+    Signal_eff->GetYaxis()->SetTitle("Eff.");
+    
+    string brejName = "B_rej";
+    TH1F *B_rej = new TH1F(brejName.c_str(),brejName.c_str(), end, HistoSignal->GetXaxis()->GetXmin(), HistoSignal->GetXaxis()->GetXmax());
+    for(unsigned int i = 0; i<end; i++){
+      B_rej->SetBinContent(i, RejectionEff_Background[i]);
+    }
+    B_rej->GetXaxis()->SetTitle(Variables[iVar].c_str());
+    B_rej->GetYaxis()->SetTitle("Rejection eff.");
+    
+    /*
+    cout << "******************************************************************" << endl;
+    cout << "Optimal cut efficiencies for each sample " << Variables[iVar] << endl;
+    if(Variables[iVar] == "NbOfSelectedJets")cout << ". Cutvalue: " << jetcut - 1 << endl;
+    if(Variables[iVar] == "NbOfSelectedBJets_CSVM")cout << ". Cutvalue: " << bcut -1 << endl;
+    cout << "******************************************************************" << endl;
+    
+    for(unsigned int i =0; i<Histo_samples.size(); i++){
+      double Nevents = -999;
+      if(Variables[iVar] == "NbOfSelectedJets") Nevents = Histo_samples[i]->Integral(jetcut,end);
+      if(Variables[iVar] == "NbOfSelectedBJets_CSVM")Nevents = Histo_samples[i]->Integral(bcut,end);
+      
+      Histo_samples[i]->Write();
+      
+      cout << Variables[iVar] << "... Efficiency for optimal cut " << Name_samples[i] << ": " << Nevents << endl;
+    }
+    */
+    
+    Histo_samples.clear();
+    Name_samples.clear();
+    cout << "done with " <<  Variables[iVar] << endl;
+  }
+  
+		
+  outputfile->Write();
+  
+  cout << "done" << endl;
+}
+
+
+
+
+
+/*
+	int end = HistoSignal->GetNbinsX();
+	
+	
+	//Efficiencies calculating as #events_passing_cut/#Total_events
+	double Total_signal;
+	double Total_background;
+	Total_signal = HistoSignal->Integral();
+	Total_background = HistoBackground->Integral();
+	
+	double * Signal_Integral_PerBin = new double [end];
+	double * Background_Integral_PerBin = new double [end];
+	double * Eff_Signal = new double [end];
+	double * RejectionEff_Background = new double [end];
+ 
+	for(int i = 0; i< end; i++){
+ double s;
+ double b;
+ 
+ s = (HistoSignal->Integral(i , end));
+ b = (HistoBackground->Integral(i,end));
+ 
+ Signal_Integral_PerBin[i] = s;
+ Background_Integral_PerBin[i] = b;
+ Eff_Signal[i] = s/Total_signal;
+ RejectionEff_Background[i] = (1- b/Total_background);
+	}
+ 
+	TGraph * Efficiencies_2D = new TGraph(end, Eff_Signal , RejectionEff_Background); // (#entries, efficiency Signal, Rejection efficiency background)
+	Efficiencies_2D->SetTitle("ROC-curve");
+	Efficiencies_2D->GetXaxis()->SetTitle("eff_s");
+	Efficiencies_2D->GetYaxis()->SetTitle("(1-eff_b)");
+	Efficiencies_2D->Write();
+	
+	
+	
+	
+	//Determine the optimal cut-value for a cut-and-count experiment
+	TH1F *Opt_cut = new TH1F("Opt_cut","Optimal cut", end, HistoSignal->GetXaxis()->GetXmin(), HistoSignal->GetXaxis()->GetXmax());
+	for(int i = 0; i<end; i++){
+ double signal_significance = Signal_Integral_PerBin[i]/sqrt(Background_Integral_PerBin[i]/*+Signal_Integral_PerBin[i]);
+ if(Background_Integral_PerBin[i] == 0) signal_significance = 1;
+	
+ Opt_cut->SetBinContent(i, signal_significance);
+	}
+	
+	Opt_cut->GetXaxis()->SetTitle(Variablename.c_str());
+ TH1F *Signal_eff = new TH1F("Signal_eff","Signal_eff", end, HistoSignal->GetXaxis()->GetXmin(), HistoSignal->GetXaxis()->GetXmax());
+	for(int i = 0; i<end; i++){
+ Signal_eff->SetBinContent(i, Eff_Signal[i]);
+	}
+	
+	Signal_eff->GetXaxis()->SetTitle(Variablename.c_str());
+ TH1F *B_rej = new TH1F("B_rej","B_rej", end, HistoSignal->GetXaxis()->GetXmin(), HistoSignal->GetXaxis()->GetXmax());
+	for(int i = 0; i<end; i++){
+ B_rej->SetBinContent(i, RejectionEff_Background[i]);
+	}
+	
+	B_rej->GetXaxis()->SetTitle(Variablename.c_str());
+	
+	cout << "Signal: " << HistoSignal->Integral(22,25) << endl;
+	cout << "Data: " <<HistoData->Integral(22,25) << endl;
+	cout << "Background: " <<HistoBackground->Integral(22,25) << endl;
+ */
